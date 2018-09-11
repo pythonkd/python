48e72e943e055bd648131f8defdc66f134355ee4
Zkp <1181878670@qq.com>
2018-09-11 22:26:55 +0800

    By Zhao Kun Peng

---
+++ 多线程/10.py
@@ -0,0 +1,42 @@
+import threading
+from time import sleep, ctime
+
+loop = [4,2]
+
+class ThreadFunc:
+
+    def __init__(self, name):
+        self.name = name
+
+    def loop(self, nloop, nsec):
+
+        #param nloop:loop函数的名称
+        #param nsec:系统休眠时间
+        #return
+        print('Start loop ', nloop, 'at', ctime())
+        sleep(nsec)
+        print('Done loop ', nloop, ' at ', ctime())
+
+    def main():
+        print('Starting at: ', ctime())
+
+        # ThreadFunc("loop")以下两个式子相等
+        # t = ThreadFunc("loop")
+        # t.loop
+        # 以下t1和t2的定义方式相等
+        t = ThreadFunc("loop")
+        t1 = threading.Thread( target = t.loop, args= ("Loop1", 4))
+        # 常见错误写法
+        # t1 = threading.Thread( target = ThreadFunc("loop").loop（100,4)
+        # t2 = threading.Thread( target = ThreadFunc("loop").loop（100,2)
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+        print("All done at:", ctime())
+
+if __name__ == '__main__':
+    main()
