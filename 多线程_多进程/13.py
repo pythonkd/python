ff4f1f2875eef7cf75c5060a87da5a582b333c6a
Zkp <1181878670@qq.com>
2018-09-19 15:53:37 +0800

    By Zhao Kun Peng

---
+++ 多线程_多进程/13.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+import os
+def info(title):
+    print(title)
+    print("module name: ",__name__)
+    print("父进程ID",os.getppid())
+    print("子进程ID",os.getpid())
+
+def f(name):
+    info("function f")
+    print('hello',name)
+
+if __name__ == '__main__':
+    info("main line")
+    p = Process(target=f, args=("bob",),name="subProcess1")
+    p.start()
+    p.join()
