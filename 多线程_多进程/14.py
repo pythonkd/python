40f68eb5a178fa1bb70766611f4e32d9323d5458
Zkp <1181878670@qq.com>
2018-09-19 16:15:03 +0800

    By Zhao Kun Peng

---
+++ 多线程_多进程/14.py
@@ -0,0 +1,26 @@
+import multiprocessing
+from time import ctime
+
+def consumer(input_q):
+    print("Into consumer:",ctime())
+    while True:
+        item = input_q.get()
+        print("pull",item,"out of q")
+        input_q.task_done()
+
+def producer(sequence,output_q):
+    print("Into producer:",ctime())
+    for item in sequence:
+        output_q.put(item)
+        print("put",item,"into q")
+    print("out of producer:",ctime())
+
+if __name__ == '__main__':
+    q = multiprocessing.JoinableQueue()
+    cons_p = multiprocessing.Process(target=consumer, args=(q,))
+    cons_p.daemon = True
+    cons_p.start()
+
+    sequence = [1,2,3,4]
+    producer(sequence, q)
+    q.join()
