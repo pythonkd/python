ec7224080d80921fab010a491ca65466edb64a6f
Zkp <1181878670@qq.com>
2018-10-30 20:23:55 +0800

    committed from zkp

---
+++ original/FIFO.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+class LastUpdateOrderedDict(OrderedDict):
+    def __init__(self, capacity):
+        super().__init__()
+        self.capacity = capacity
+    def __setitem__(self, key, value):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove', last)
+        if containsKey:
+            del self[key]
+            print("'set", (key, value))
+        else:
+            print("add", (key, value))
+        OrderedDict.__setitem__(self, key, value)
