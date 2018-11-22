969408ac8cdad415ef0964f3fcbdbc5fd2088d88
Zkp <1181878670@qq.com>
2018-11-22 15:23:49 +0800

    committed from zkp

---
+++ FluentPython/deco.py
@@ -0,0 +1,13 @@
+def deco(func):
+    def inner():
+        print("running inner()")
+
+    return inner
+
+
+@deco
+def target():
+    print("running target")
+
+if __name__ == '__main__':
+    target()
