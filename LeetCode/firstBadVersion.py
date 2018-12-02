aece3b92c4d80c04c2b24aa17a82ebd0a472a92a
Zkp <1181878670@qq.com>
2018-12-02 22:35:34 +0800

    committed from zkp

---
+++ LeetCode/firstBadVersion.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l = 1
+        r = n
+        while l < r:
+            mid = (l+r)//2
+            # print(isBadVersion(mid))
+            if not isBadVersion(mid):
+                l = mid + 1
+            else:
+                r = mid
+        return l
+    
