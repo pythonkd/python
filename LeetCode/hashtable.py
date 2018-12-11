45baada519a5bdbb183a4283c5c7d2f0d367cf41
Zkp <1181878670@qq.com>
2018-12-11 15:21:42 +0800

    committed from zkp

--- LeetCode/hashtable.py
+++ LeetCode/hashtable.py
@@ -1,95 +1,82 @@
 class Array(object):
-    def __init__(self, size=30, init=None):
+    def __init__(self, size=32, init=None):
         self.size = size
-        self.items = [init]*size
+        self.items = [init] * size
+    def clear(self, value=None):
+        for i in range(len(self)):
+            self.items[i] = value
+    def __len__(self):
+        return self.size
     def __getitem__(self, index):
         return self.items[index]
     def __setitem__(self, index, value):
         self.items[index] = value
-    def __len__(self):
-        return self.size
-    def clear(self):
-        for i in range(len(self.size)):
-            self.items[i] = init
     def __iter__(self):
-        for i in self.items:
-            yield i
+        for item in self.items:
+            yield item
 class Slot(object):
-    def __init__(self, value=None, key=None):
+    def __init__(self, key=None, value=None):
         self.key, self.value = key, value
 class HashTable(object):
-    EMPTY = None
     UNUSED = None
+    EMPTY = Slot()
     def __init__(self):
-        self._table = Array(20, HashTable.UNUSED)
+        self._table = Array(8, init=HashTable.UNUSED)
         self.length = 0
-    @property
-    def _load_factor(self):
-        return self.length / len(self._table)
     def __len__(self):
         return self.length
     def _hash(self, key):
-        return abs(hash(key)%len(self._table))
-    def _find_key(self, key):
+        return abs(hash(key)) % len(self._table)
+    @property
+    def load_factor(self):
+        return self.length / len(self._table)
+    def find_key(self, key):
         index = self._hash(key)
-        _len = self.length
         while self._table[index] is not HashTable.UNUSED:
-            if self._table[index] is HashTable.EMPTY:
-                index = (index*5 +1) % _len
-                continue
-            elif self._table[index].key == key:
+            if self._table[index] is not HashTable.EMPTY and self._table[index].key == key:
                 return index
-            else:
-                index = (index *5 + 1) / _len
+            index = (index*5+1)%len(self._table)
         return None
-    def _slot_can_insert(self, index):
-        return (self._table[index] is HashTable.EMPTY or HashTable.UNUSED)
-    def _find_slots_for_insert(self, key):
+    def find_key_to_insert(self, key):
         index = self._hash(key)
-        _len = self.length
-        while not _slot_can_insert(index):
-            index = (index*5 +1)%_len
+        while self._table[index] not in (HashTable.UNUSED, HashTable.EMPTY):
+            index = (index*5+1) % len(self._table)
         return index
-    def __contains__(self, key):
-        index = _find_key(key)
-        return index is not None
     def add(self, key, value):
-        if key in self:
-            index = self._find_key(key)
+        if self.find_key(key):
             self._table[index].value = value
-            return False
         else:
-            index = _find_slots_for_insert(key)
-            self._table[index] = Slot(value, key)
-            self.length += 1
-            if self._load_factor() >= 0.8:
-                self._rehash()
-            return True
-    def rehash(self):
+            index = self.find_key_to_insert(key)
+            slot = Slot(key, value)
+            self._table[index] = slot
+        self.length += 1
+        if self.load_factor >= 0.8:
+            self._rehash_table()
+    def _rehash_table(self):
         old_table = self._table
-        newsize = len(self) * 2
+        newsize = len(self._table) * 2
         self._table = Array(newsize, HashTable.UNUSED)
-        self.length = 0
-        for slot in old_table:
-            if slot is not HashTable.UNUSED and slot is not HashTable.EMPTY:
-                index = self._find_slots_for_insert(slot.key)
-                self._table[index] = slot
-                self.length += 1
-    def get(self, key, default=None):
-        index = self._find_key(key)
-        if index is None:
-            return default
-        else:
-            return self._table[index].value
+        for i in old_table:
+            if i not in (HashTable.UNUSED, HashTable.EMPTY):
+                index = self.find_key_to_insert(i.key)
+                self._table[index] = i
     def remove(self, key):
-        index = self._find_key(key)
-        if index is None:
-            raise KeyError()
-        value = self._table[index].value
-        self._table[index].key = HashTable.EMPTY
-        self.length -= 1
-        return value
+        index = self.find_key(key)
+        if index is not None:
+            self._table[index] = HashTable.EMPTY
+            self.length -= 1
+        else:
+            raise KeyError('Not Found')
     def __iter__(self):
-        for slot in self._table:
-            if slot not in (HashTable.EMPTY, HashTable.UNUSED):
-                yield slot.key
+        for item in self._table:
+            if item not in (HashTable.UNUSED, HashTable.EMPTY):
+                yield (item.key, item.value)
+    def __contains__(self, key):
+        if self.find_key(key):
+            return True
+        return False
+    def get(self, key, default=None):
+        index = self.find_key(key)
+        if  index is not None:
+            return self._table[index].value
+        return default
